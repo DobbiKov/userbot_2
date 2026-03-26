db6b4936da22a7adf44917a2dedaf2b71ebafab2
Yehor Korotenko <dobbikov@gmail.com>
2026-03-26 13:44:26 +0100

    feat: added location to config

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ config.read("config.ini")
 api_id = config.get("pyrogram", "api_id")
 api_hash = config.get("pyrogram", "api_hash")
 gemini_api = config.get("pyrogram", "gemini_api")
+location = config.get("user", "location")
 
 querty_id = 5200613842
 me_id = 716720991 
